049e313102589329c23eadee99f5e01dca8143e0
Evan Mattson <me@aaemnnost.tv>
2017-12-10 20:12:49 +0200

    move functions to file

--- src/build.php
+++ src/build.php
@@ -2,11 +2,8 @@
 
 namespace Build;
 
-use \Comodojo\Zip\Zip;
 use Illuminate\Container\Container;
 use PHPGit\Git;
-use Symfony\Component\Filesystem\Filesystem;
-use Symfony\Component\Finder\Finder;
 use Composer\Semver\VersionParser;
 
 require_once dirname(__DIR__) . '/vendor/autoload.php';
@@ -37,75 +34,6 @@ $package = json_decode(file_get_contents("{$paths->root}/template/composer.json"
 $container->instance('package', $package);
 $build_version = $package->extra->{"wp-phpunit"}->version;
 
-/**
- * Initialize a new branch for the given package version.
- *
- * @param \stdClass $meta
- * @param string $starting
- */
-function init_branch($meta, $starting = 'HEAD') {
-    Container::getInstance()
-        ->get('repos')
-        ->package
-        ->branch
-        ->create("tree-{$meta->major_version}", $starting)
-    ;
-}
-
-/**
- * Build the package for the given version.
- *
- * @param \stdClass $meta
- * @param string $package_version
- */
-function build($meta, $package_version = null) {
-    echo "\n\nBuilding $meta->package_version\n";
-
-    $package_version = $package_version ?: $meta->package_version;
-    $repos = Container::getInstance()->get('repos');
-    $paths = Container::getInstance()->get('paths');
-    $fs = new Filesystem;
-    
-    if ($fs->exists($paths->build)) {
-        $fs->remove($paths->build);
-    }
-
-    $fs->mkdir("{$paths->build}/uncompressed/", 0755);
-    $repos->wordpress->archive("{$paths->build}/source.zip", "{$meta->sha}:tests/phpunit", null, ['format' => 'zip']);
-    Zip::open("{$paths->build}/source.zip")->extract("{$paths->build}/uncompressed/");
-
-    $repos->package->checkout("tree-{$meta->major_version}");
-    $repos->package->reset->hard();
-    $repos->package->run($repos->package->getProcessBuilder()->add('clean')->add('-df')->getProcess());
-
-    collect((new Finder)->directories()->in("{$paths->build}/uncompressed/")->depth(0)->exclude(['build','tests']))->each(function($dir) use ($fs, $paths) {
-        echo "Mirroring directory ", $dir->getRelativePathname(), "\n";
-        $fs->mirror($dir->getRealPath(), $paths->package . '/' . $dir->getRelativePathname());
-    });
-
-    collect((new Finder)->files()->in($paths->template))->each(function($file) use ($fs, $paths) {
-        echo "Copying file ", $file->getRelativePathname(), "\n";
-        $fs->copy($file->getRealPath(), $paths->package . '/' . $file->getRelativePathname());
-    });
-
-    try {
-        $repos->package->add('.');
-        
-        // If we try to commit without any changes, a GitException will be thrown.
-        if (0 < count($repos->package->status()['changes'])) {
-            $repos->package->commit("Building $package_version", ['all' => true]);
-        }
-
-        $repos->package->tag->create($package_version);
-    } catch (\Exception $e) {
-        abort($e);
-    }
-}
-
-function abort($exception) {
-    var_dump($exception);
-    exit('1');
-}
 
 // Crawl tags
 collect($repos->wordpress->tag())->reject(function($tag) {
---
+++ src/functions.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace Build;
+
+use Comodojo\Zip\Zip;
+use Illuminate\Container\Container;
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\Finder\Finder;
+use Symfony\Component\Finder\SplFileInfo;
+
+/**
+ * Initialize a new branch for the given package version.
+ *
+ * @param \stdClass $meta
+ * @param string $starting
+ */
+function init_branch($meta, $starting = 'HEAD') {
+	Container::getInstance()
+	         ->get('repos')
+		->package
+		->branch
+		->create("tree-{$meta->major_version}", $starting)
+	;
+}
+
+/**
+ * Build the package for the given version.
+ *
+ * @param \stdClass $meta
+ */
+function build($meta) {
+	echo "\n\nBuilding $meta->tag\n";
+
+	$repos = Container::getInstance()->get('repos');
+	$paths = Container::getInstance()->get('paths');
+	$fs = new Filesystem;
+
+	if ($fs->exists($paths->build)) {
+		$fs->remove($paths->build);
+	}
+
+	$fs->mkdir("{$paths->build}/uncompressed/", 0755);
+	$repos->wordpress->archive("{$paths->build}/source.zip", "{$meta->sha}:tests/phpunit", null, ['format' => 'zip']);
+	Zip::open("{$paths->build}/source.zip")->extract("{$paths->build}/uncompressed/");
+
+	$repos->package->checkout("tree-{$meta->major_version}");
+	$repos->package->reset->hard();
+	$repos->package->run($repos->package->getProcessBuilder()->add('clean')->add('-df')->getProcess());
+
+	collect((new Finder)->directories()->in("{$paths->build}/uncompressed/")->depth(0)->exclude(['tests']))
+		->each(function(SplFileInfo $dir) use ($fs, $paths) {
+			echo "Mirroring directory ", $dir->getRelativePathname(), "\n";
+			$fs->mirror($dir->getRealPath(), $paths->package . '/' . $dir->getRelativePathname());
+		});
+
+	collect((new Finder)->files()->in($paths->template))
+		->each(function(SplFileInfo $file) use ($fs, $paths) {
+			echo "Copying file ", $file->getRelativePathname(), "\n";
+			$fs->copy($file->getRealPath(), $paths->package . '/' . $file->getRelativePathname());
+		});
+
+	$repos->package->add('.');
+
+	// If we try to commit without any changes, a GitException will be thrown.
+	if (0 < count($repos->package->status()['changes'])) {
+		$repos->package->commit("Building $meta->tag", ['all' => true]);
+	}
+
+	$repos->package->tag->create($meta->tag);
+}
