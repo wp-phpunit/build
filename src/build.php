df4d2f692af1394665ec864115d24e2a7fae45d4
Evan Mattson <me@aaemnnost.tv>
2017-12-10 20:15:27 +0200

    replace references to package version with tag

--- src/build.php
+++ src/build.php
@@ -30,36 +30,28 @@ $container->instance('repos', $repos);
 $container->singleton('version_parser', VersionParser::class);
 Container::setInstance($container);
 
-$package = json_decode(file_get_contents("{$paths->root}/template/composer.json"));
-$container->instance('package', $package);
-$build_version = $package->extra->{"wp-phpunit"}->version;
-
-
-// Crawl tags
 collect($repos->wordpress->tag())->reject(function($tag) {
     return version_compare($tag, '3.7', '<'); // skip tags before phpunit library was added in 3.7
-})->map(function($tag) use ($repos, $build_version) {
+})->map(function($tag) use ($repos) {
+    /* @var Git $wp */
     $wp = $repos->wordpress;
-    $package_version = $build_version ? "{$tag}-patch{$build_version}" : $tag;
     $version_parser = Container::getInstance()->make('version_parser');
-    $major_version = collect(explode('.', $package_version))->take(2)->implode('.');
+    $major_version = collect(explode('.', $tag))->take(2)->implode('.');
 
     return (object) [
         'tag' => $tag,
         'tag_normalized' => $version_parser->normalize($tag),
-        'sha' => trim(
-            $wp->run($wp->getProcessBuilder()->add('rev-parse')->add($tag)->getProcess())
-        ),
         'major_version' => $major_version,
         'major_version_normalized' => $version_parser->normalize($major_version),
-        'package_version' => $package_version,
-        'package_version_normalized' => $version_parser->normalize($package_version),
+        'sha' => trim(
+	        $wp->run($wp->getProcessBuilder()->add('rev-parse')->add($tag)->getProcess())
+        ),
     ];
 })->sortBy('tag_normalized')->reject(function($meta) use ($repos) {
-    return collect($repos->package->tag())->contains($meta->package_version); // remove any which already exist on the target repository
+    return collect($repos->package->tag())->contains($meta->tag); // remove any which already exist on the target repository
 })->each(function($meta) use ($container, $repos) {
     $major_branch = "tree-{$meta->major_version}";
-    $is_dot_zero = version_compare($meta->major_version_normalized, $meta->package_version_normalized, '=');
+    $is_dot_zero = version_compare($meta->major_version_normalized, $meta->tag_normalized, '=');
     $branch_exists = collect($repos->package->branch())->contains('name', $major_branch);
 
     if ($is_dot_zero && ! $branch_exists) {
@@ -67,12 +59,8 @@ collect($repos->wordpress->tag())->reject(function($tag) {
         init_branch($meta, 'master');
         build($meta);
 
-        try {
-            $repos->package->checkout('master');
-            $repos->package->merge($major_branch, null, ['no-ff' => true]);
-        } catch(\Exception $e) {
-            abort($e);
-        }
+        $repos->package->checkout('master');
+        $repos->package->merge($major_branch, null, ['no-ff' => true]);
     } else {
         build($meta);
     }
